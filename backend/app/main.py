b5cef2457dcd21b6ab642e9cbeb12b823363a9ec
yeyhans <yeysonhans@gmail.com>
2025-01-20 02:03:37 -0300

    proyecto virgen con documentacion

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
-from app.api import openai  # Asegúrate de que 'app' está siendo importado correctamente
-from app.api.supabase import router as supabase_router
 
-# Inicializar la aplicación FastAPI
 app = FastAPI()
 
-# Incluir las rutas desde el router de OpenAI
-app.include_router(openai.router)  # Asegúrate de que 'openai.router' esté definido
-app.include_router(supabase_router)
+
+@app.get("/")
+def hello_world():
+    return {"message": "OKsss"}
