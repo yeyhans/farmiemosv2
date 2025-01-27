e3323f397e2120ed5c7fc6c63540d6fe5ef9abe4
yeyhans <yeysonhans@gmail.com>
2025-01-26 22:42:31 -0300

    vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from '@astrojs/tailwind';
-
-
 import vercel from '@astrojs/vercel';
-
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://farmiemosv2.vercel.app/',
   integrations: [mdx(), sitemap(), tailwind()],
-  output: 'static',
+  output: 'server',
   adapter: vercel()
 });
